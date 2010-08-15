a69f4be0b4470b72b17392703ba0d8a7a46afb9a
John SJ Anderson <genehack@genehack.org>
2010-08-15 16:44:05 -0400

    WIP

---
+++ t/lib/Test/WWW/Fitbit/API.pm
@@ -0,0 +1,58 @@
+package Test::WWW::Fitbit::API;
+use strict;
+use warnings;
+
+use base qw/ Test::Class /;
+
+use Test::More;
+
+use File::Slurp;
+use FindBin;
+use LWP::UserAgent;
+use Test::MockObject::Extends;
+
+use WWW::Fitbit::API;
+
+sub setup_fitbit_object :Test(setup) {
+  my $test = shift;
+
+  my $browser = LWP::UserAgent->new();
+  my $mock_browser = Test::MockObject::Extends->new( $browser );
+
+  $mock_browser->mock( 'get' , sub {
+    my( $self , $url ) = @_;
+    my( $type ) = $url =~ /type=([^&]+)&/ or die $url;
+
+    my $file = "$FindBin::Bin/data/$type.xml";
+    die "Can't find $file" unless -e $file;
+
+    my $text = read_file( $file );
+    my $response = HTTP::Response->new();
+    my $mock_response = Test::MockObject::Extends->new( $response );
+    $mock_response->mock( 'content' , sub { return $text } )
+                  ->mock( 'is_success' , sub { return 1 } );
+    return $mock_response;
+  });
+
+  $test->{fb} = WWW::Fitbit::API->new({ _set_browser => $mock_browser });
+}
+
+sub isa_fitbit :Test(1) {
+  my $test = shift;
+
+  isa_ok( $test->{fb} , 'WWW::Fitbit::API' );
+}
+
+sub active_score :Test(1) {
+  my $test = shift;
+
+  is( $test->{fb}->active_score , '52.0' );
+}
+
+sub weight :Test(1) {
+  my $test = shift;
+
+  is( $test->{fb}->weight , '195.6' );
+}
+
+1;
